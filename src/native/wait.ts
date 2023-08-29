4118ae2eda02c62c799845ada5d95d0808a6ddbb
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2023-08-29 13:16:44 +0200

    Added more functions uwu

---
+++ src/native/wait.ts
@@ -0,0 +1,22 @@
+import { setTimeout } from "timers/promises"
+import { ArgType, NativeFunction, Return } from "../structures"
+
+export default new NativeFunction({
+    name: "$wait",
+    description: "Delays the code below for x milliseconds",
+    brackets: true,
+    unwrap: true,
+    args: [
+        {
+            name: "ms",
+            description: "The ms to wait for",
+            rest: false,
+            type: ArgType.Number,
+            required: true
+        }
+    ],
+    async execute(ctx, [ ms ]) {
+        await setTimeout(ms)
+        return Return.success()
+    },
+})
