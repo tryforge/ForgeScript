20ba209a0fce1584356af7fb67ac52ce3bd08dd5
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2023-09-12 15:53:26 +0200

    added message exists and added fields to reply

--- src/native/messageCreatedAt.ts
+++ src/native/messageCreatedAt.ts
@@ -1,12 +1,31 @@
-import { MessageType } from "discord.js"
-import { NativeFunction, Return } from "../structures"
+import { BaseChannel, MessageType } from "discord.js"
+import { ArgType, NativeFunction, Return } from "../structures"
 
 export default new NativeFunction({
     name: "$messageCreatedAt",
     version: "1.0.2",
     description: "Returns the timestamp of the message",
-    unwrap: false,
-    execute(ctx) {
-        return Return.success(MessageType[ctx.message?.createdTimestamp!])
+    unwrap: true,
+    brackets: false,
+    args: [
+        {
+            name: "channel ID",
+            rest: false,
+            required: true,
+            description: "The channel to get the message from",
+            type: ArgType.Channel,
+            check: (i: BaseChannel) => i.isTextBased()
+        },
+        {
+            name: "message ID",
+            description: "The message to get its timestamp",
+            rest: false,
+            type: ArgType.Message,
+            pointer: 0,
+            required: true
+        }
+    ],
+    execute(ctx, [, message ]) {
+        return Return.success(MessageType[(message ?? ctx.message)?.createdTimestamp!])
     },
 })
