6ef963c5f4f97193aafeecbc5b7bbf53cc662187
Nicky <111157596+xNickyDev@users.noreply.github.com>
2023-09-23 20:59:51 +0200

    Update voiceStateUpdate.ts

--- src/handlers/events/voiceStateUpdate.ts
+++ src/handlers/events/voiceStateUpdate.ts
@@ -4,7 +4,7 @@ import { DiscordEventHandler } from "../../structures/DiscordEventHandler"
 export default new DiscordEventHandler({
     name: "voiceStateUpdate",
     version: "1.0.1",
-    description: "This event is fired when an user joins/leaves a voice channel",
+    description: "This event is fired when a user joins/leaves a voice channel",
     listener: async function (old, newer) {
         const commands = this.commands.get("voiceStateUpdate")
 
