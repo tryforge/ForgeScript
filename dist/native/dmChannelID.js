c39eb8f1414a754c91e02fcde6265f26b26ffd09
Nicky <111157596+xNickyDev@users.noreply.github.com>
2023-09-23 23:27:35 +0200

    Update dmChannelID.js

--- dist/native/dmChannelID.js
+++ dist/native/dmChannelID.js
@@ -8,7 +8,7 @@ const structures_1 = require("../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$dmChannelID",
     version: "1.0.0",
-    description: "Returns the dm channel id of an user",
+    description: "Returns the dm channel id of a user",
     brackets: false,
     unwrap: true,
     args: [
