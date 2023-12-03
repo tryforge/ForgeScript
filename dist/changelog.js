845686a52e9df98bcb4a01af7cd982c520615561
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2023-12-03 15:48:28 +0100

    ^Added^ bufferReadInt32^ and^ bufferWriteInt32^

--- dist/changelog.js
+++ dist/changelog.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const fs_1 = require("fs");
 const process_1 = require("process");
-const path = "./docs/changelog";
+const path = "./changelog";
 if (!(0, fs_1.existsSync)(path))
     (0, fs_1.mkdirSync)(path);
 const version = require("../package.json").version;
@@ -12,5 +12,5 @@ const fileName = `${path}-${version}.txt`;
 const logs = (0, fs_1.existsSync)(fileName) ? (0, fs_1.readFileSync)(path, "utf-8").split("\n") : new Array();
 logs.unshift(msg);
 (0, fs_1.writeFileSync)(fileName, logs.join("\n"));
-(0, child_process_1.execSync)("git add . && git commit -m \" " + msg + "\" &6 git push -u origin dev");
+(0, child_process_1.execSync)("git add . && git commit -m \" " + msg + "\" && git push -u origin dev");
 //# sourceMappingURL=changelog.js.map
--- src/changelog.ts
+++ src/changelog.ts
@@ -2,7 +2,7 @@ import { execSync } from "child_process"
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs"
 import { argv } from "process"
 
-const path = "./docs/changelog"
+const path = "./changelog"
 if (!existsSync(path)) mkdirSync(path)
 
 const version = require("../package.json").version
@@ -12,4 +12,4 @@ const fileName = `${path}-${version}.txt`
 const logs = existsSync(fileName) ? readFileSync(path, "utf-8").split("\n") : new Array<string>()
 logs.unshift(msg)
 writeFileSync(fileName, logs.join("\n"))
-execSync("git add . && git commit -m \" " + msg + "\" &6 git push -u origin dev")
+execSync("git add . && git commit -m \" " + msg + "\" && git push -u origin dev")
