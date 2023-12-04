9a9dd4cdab2ea483e6bbd6254c7ef6f3cb23333b
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2023-12-04 20:14:14 +0100

    ^Added^ lastMessageID^

--- dist/native/emojiID.js
+++ dist/native/emojiID.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const structures_1 = require("../structures");
 exports.default = new structures_1.NativeFunction({
-    name: "$emojiName",
+    name: "$emojiID",
     description: "Returns the emoji id",
     brackets: false,
     unwrap: true,
