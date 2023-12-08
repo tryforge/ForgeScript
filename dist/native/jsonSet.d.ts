223f3c7e9278f60b28e00c80fb8105ec6145deaf
Plinkie03 <rubenellolkito@gmail.com>
2023-12-08 13:17:46 +0100

    Added jsonSet

---
+++ dist/native/jsonSet.d.ts
@@ -0,0 +1,16 @@
+import { ArgType, NativeFunction } from "../structures";
+declare const _default: NativeFunction<[{
+    name: string;
+    description: string;
+    rest: false;
+    required: true;
+    type: ArgType.String;
+}, {
+    name: string;
+    description: string;
+    type: ArgType.Json;
+    rest: false;
+    required: true;
+}], true>;
+export default _default;
+//# sourceMappingURL=jsonSet.d.ts.map
