0973dc53d38e9f3178ef10e58515ae91be0c5a22
Plinkie03 <rubenellolkito@gmail.com>
2023-12-10 12:21:01 +0100

    Speeded up interpreter performance

--- dist/core/Interpreter.js
+++ dist/core/Interpreter.js
@@ -27,7 +27,7 @@ class Interpreter {
             const rt = await fn.execute(ctx);
             if (!rt.success && !ctx.handleNotSuccess(rt))
                 return null;
-            args[i] = fn.negated ? null : rt.value;
+            args[i] = fn.data.negated ? null : rt.value;
         }
         const content = runtime.data.resolve(args);
         if (!runtime.doNotSend) {
