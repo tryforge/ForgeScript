31d4d796e9544a949fdea615dc953204a5affc24
Plinkie03 <rubenellolkito@gmail.com>
2023-12-22 18:57:53 +0100

    Added $hyperlink

--- dist/native/mentionedChannelCount.js
+++ dist/native/mentionedChannelCount.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const structures_1 = require("../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$mentionedChannelCount",
+    version: "1.3.0",
     description: "Returns the mentioned channel count",
     unwrap: false,
     execute(ctx) {
