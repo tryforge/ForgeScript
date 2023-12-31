09f1d49d5ab576c426b2df8f0952ec5417688f02
Plinkie03 <rubenellolkito@gmail.com>
2023-12-31 18:31:40 +0100

    Commented all runnable properties

--- dist/core/Interpreter.d.ts
+++ dist/core/Interpreter.d.ts
@@ -23,16 +23,49 @@ export type States = {
     };
 };
 export interface IRunnable {
+    /**
+     * The available discord client
+     */
     client: ForgeClient;
+    /**
+     * The compiled data to execute
+     */
     data: IExtendedCompilationResult;
+    /**
+     * The context this code will run in
+     */
     obj: Sendable;
+    /**
+     * The command used for this execution
+     */
     command: BaseCommand<unknown> | null;
+    /**
+     * Whether to suppress sending the response to discord.
+     */
     doNotSend?: boolean;
+    /**
+     * Extras data
+     */
     extras?: unknown;
+    /**
+     * Whether to suppress errors from being sent to discord, and be sent to console instead
+     */
     redirectErrorsToConsole?: boolean;
+    /**
+     * The old and new states of a event
+     */
     states?: States;
+    /**
+     * The already existing variables defined with $let
+     */
     keywords?: Record<string, string>;
+    /**
+     * The already existing env variables
+     */
     environment?: Record<string, unknown>;
+    /**
+     * The args used in the message command
+     */
     args?: string[];
 }
 export declare class Interpreter {
