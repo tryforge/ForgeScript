d7e7b50ed1857fd5b2c1773033c554e2bf46fcac
Plinkie03 <rubenellolkito@gmail.com>
2024-01-05 22:12:41 +0100

    Added $editField

--- dist/native/guild/guildHasFeatures.js
+++ dist/native/guild/guildHasFeatures.js
@@ -4,6 +4,7 @@ const discord_js_1 = require("discord.js");
 const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$guidHasFeatures",
+    version: "1.4.0",
     description: "Returns whether this guild has all the given features",
     unwrap: true,
     brackets: true,
