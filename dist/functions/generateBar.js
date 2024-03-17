c498da551843c2e152da0e3ca78806644333cbd3
Plinkie03 <rubenellolkito@gmail.com>
2024-03-18 00:37:27 +0100

    Fixed $bar again

--- dist/functions/generateBar.js
+++ dist/functions/generateBar.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateBar = void 0;
 function generateBar(current, max, len = 10, fill = "█", empty = "▒") {
     const fillN = Math.round(Math.min(current, max) / max * len);
-    return fill.repeat(fillN).padEnd((len * empty.length + fillN), empty);
+    return fill.repeat(fillN) + empty.repeat(len - fillN);
 }
 exports.generateBar = generateBar;
 //# sourceMappingURL=generateBar.js.map
--- src/functions/generateBar.ts
+++ src/functions/generateBar.ts
@@ -1,4 +1,4 @@
 export function generateBar(current: number, max: number, len: number = 10, fill: string = "█", empty: string = "▒") {
     const fillN = Math.round(Math.min(current, max) / max * len)
-    return fill.repeat(fillN).padEnd((len * empty.length + fillN), empty)
+    return fill.repeat(fillN) + empty.repeat(len - fillN)
 }
