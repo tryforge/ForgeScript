fbc1b110ced6776bb0d5348c39437674b8a6f9f9
Nicky <111157596+xNickyDev@users.noreply.github.com>
2024-09-01 19:44:15 +0000

    Added $userDefaultAvatar

---
+++ dist/native/user/userDefaultAvatar.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$userDefaultAvatar",
+    version: "1.5.0",
+    description: "Returns the default user avatar",
+    brackets: false,
+    output: structures_1.ArgType.URL,
+    args: [
+        {
+            name: "user ID",
+            description: "The user to retrieve the default avatar",
+            rest: false,
+            required: true,
+            type: structures_1.ArgType.User,
+        },
+    ],
+    unwrap: true,
+    execute(ctx, [user]) {
+        return this.success((user ?? ctx.user).defaultAvatarURL);
+    },
+});
+//# sourceMappingURL=userDefaultAvatar.js.map
---
+++ src/native/user/userDefaultAvatar.ts
@@ -0,0 +1,23 @@
+import { ImageExtension, ImageSize } from "discord.js"
+import { ArgType, NativeFunction, Return } from "../../structures"
+
+export default new NativeFunction({
+    name: "$userDefaultAvatar",
+    version: "1.5.0",
+    description: "Returns the default user avatar",
+    brackets: false,
+    output: ArgType.URL,
+    args: [
+        {
+            name: "user ID",
+            description: "The user to retrieve the default avatar",
+            rest: false,
+            required: true,
+            type: ArgType.User,
+        },
+    ],
+    unwrap: true,
+    execute(ctx, [user]) {
+        return this.success((user ?? ctx.user).defaultAvatarURL)
+    },
+})
