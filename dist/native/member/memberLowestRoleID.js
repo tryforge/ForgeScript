78415d3fb38fe39b7d75fea3ddce5bfe35ffc99c
Nicky <111157596+xNickyDev@users.noreply.github.com>
2024-09-06 18:24:38 +0000

    'Added '

---
+++ dist/native/member/memberLowestRoleID.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$memberLowestRoleID",
+    version: "1.5.0",
+    description: "Returns the lowest role id of a member",
+    unwrap: true,
+    output: structures_1.ArgType.Role,
+    brackets: false,
+    args: [
+        {
+            name: "guild ID",
+            description: "The guild to pull member from",
+            rest: false,
+            type: structures_1.ArgType.Guild,
+            required: true,
+        },
+        {
+            name: "user ID",
+            pointer: 0,
+            description: "The user to get its lowest role id",
+            rest: false,
+            type: structures_1.ArgType.Member,
+            required: true,
+        },
+    ],
+    execute(ctx, [guild, member]) {
+        guild ??= ctx.guild;
+        member ??= ctx.member;
+        const lowest = member.roles.cache.filter(role => role.id !== guild.id).sort((a, b) => a.position - b.position).first();
+        return this.success(lowest?.id ?? guild.id);
+    },
+});
+//# sourceMappingURL=memberLowestRoleID.js.map
---
+++ src/native/member/memberLowestRoleID.ts
@@ -0,0 +1,34 @@
+import { ArgType, NativeFunction, Return } from "../../structures"
+
+export default new NativeFunction({
+    name: "$memberLowestRoleID",
+    version: "1.5.0",
+    description: "Returns the lowest role id of a member",
+    unwrap: true,
+    output: ArgType.Role,
+    brackets: false,
+    args: [
+        {
+            name: "guild ID",
+            description: "The guild to pull member from",
+            rest: false,
+            type: ArgType.Guild,
+            required: true,
+        },
+        {
+            name: "user ID",
+            pointer: 0,
+            description: "The user to get its lowest role id",
+            rest: false,
+            type: ArgType.Member,
+            required: true,
+        },
+    ],
+    execute(ctx, [guild, member]) {
+        guild ??= ctx.guild!
+        member ??= ctx.member!
+        const lowest = member.roles.cache.filter(role => role.id !== guild.id).sort((a, b) => a.position - b.position).first()
+
+        return this.success(lowest?.id ?? guild.id)
+    },
+})
