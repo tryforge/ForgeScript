40d1fc37221da0f1fb479bff6c5d4dda31a3cb86
xNickyDev <xnickydev@gmail.com>
2024-10-10 00:00:26 +0200

    Added $isPinned

---
+++ dist/native/message/isPinned.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$isPinned",
+    description: "Returns whether the message is pinned",
+    aliases: [
+        "$isMessagePinned",
+        "$messagePinned"
+    ],
+    unwrap: true,
+    brackets: false,
+    args: [
+        {
+            name: "channel ID",
+            rest: false,
+            required: true,
+            description: "The channel to pull message from",
+            type: structures_1.ArgType.Channel,
+            check: (i) => i.isTextBased(),
+        },
+        {
+            name: "message ID",
+            pointer: 0,
+            description: "The message to check if its pinned",
+            rest: false,
+            required: true,
+            type: structures_1.ArgType.Message,
+        }
+    ],
+    output: structures_1.ArgType.Boolean,
+    async execute(ctx, [, message]) {
+        return this.success((message ?? ctx.message)?.pinned ?? false);
+    },
+});
+//# sourceMappingURL=isPinned.js.map
---
+++ src/native/message/isPinned.ts
@@ -0,0 +1,36 @@
+import { BaseChannel } from "discord.js"
+import { ArgType, NativeFunction, Return } from "../../structures"
+
+export default new NativeFunction({
+    name: "$isPinned",
+    version: "1.5.0",
+    description: "Returns whether the message is pinned",
+    aliases: [
+        "$isMessagePinned",
+        "$messagePinned"
+    ],
+    unwrap: true,
+    brackets: false,
+    args: [
+        {
+            name: "channel ID",
+            rest: false,
+            required: true,
+            description: "The channel to pull message from",
+            type: ArgType.Channel,
+            check: (i: BaseChannel) => i.isTextBased(),
+        },
+        {
+            name: "message ID",
+            pointer: 0,
+            description: "The message to check if its pinned",
+            rest: false,
+            required: true,
+            type: ArgType.Message,
+        }
+    ],
+    output: ArgType.Boolean,
+    async execute(ctx, [, message]) {
+        return this.success((message ?? ctx.message)?.pinned ?? false)
+    },
+})
