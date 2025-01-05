aad682a6a00d52229ab585d3c0a46947cd157dc4
aggelos-007 <aggeloss007@gmail.com>
2025-01-05 17:48:18 +0200

    update npm and upload some dist

--- dist/native/embed/deleteField.js
+++ dist/native/embed/deleteField.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$deleteField",
+    version: "2.1.0",
     description: "Deletes an embed field",
     unwrap: true,
     args: [
