62089c04d28b46f363ec5e2f186ea4981f39b4dd
xNickyDev <xnickydev@gmail.com>
2025-01-25 19:00:40 +0100

    Added optional guild arg to $applicationCommands

--- dist/native/bot/applicationCommands.js
+++ dist/native/bot/applicationCommands.js
@@ -6,9 +6,19 @@ exports.default = new structures_1.NativeFunction({
     version: "1.5.0",
     description: "Returns all application commands",
     output: structures_1.ArgType.Json,
-    unwrap: false,
-    async execute(ctx) {
-        const commands = await ctx.client.application.commands.fetch().catch(ctx.noop);
+    unwrap: true,
+    brackets: false,
+    args: [
+        {
+            name: "guild ID",
+            description: "The guild to get application commands from",
+            rest: false,
+            required: true,
+            type: structures_1.ArgType.Guild,
+        },
+    ],
+    async execute(ctx, [guild]) {
+        const commands = await ctx.client.application.commands.fetch({ guildId: guild?.id }).catch(ctx.noop);
         return this.successJSON(commands);
     },
 });
