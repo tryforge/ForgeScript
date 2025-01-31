e1ccc04d9e676c7cd77e695adc1f48ccfa22c632
xNickyDev <xnickydev@gmail.com>
2025-01-31 12:18:26 +0100

    Added $fetchMessage

---
+++ dist/native/message/fetchMessage.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$fetchMessage",
+    description: "Fetches all data of a message",
+    unwrap: true,
+    brackets: false,
+    args: [
+        {
+            name: "channel ID",
+            description: "The channel to pull message from",
+            rest: false,
+            required: true,
+            type: structures_1.ArgType.TextChannel,
+        },
+        {
+            name: "message ID",
+            description: "The message to fetch its data",
+            rest: false,
+            required: true,
+            pointer: 0,
+            type: structures_1.ArgType.Message,
+        },
+    ],
+    async execute(ctx, [, message]) {
+        await (message ?? ctx.message)?.fetch();
+        return this.success();
+    },
+});
+//# sourceMappingURL=fetchMessage.js.map
