30110d8aa8d8b596d3fc77e8f76ab3e9ade1c84e
xNickyDev <xnickydev@gmail.com>
2025-02-12 15:43:04 +0100

    Added $djsVersion

--- dist/native/guild/guildInvitesDisabledUntil.js
+++ dist/native/guild/guildInvitesDisabledUntil.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$guildInvitesDisabledUntil",
+    version: "2.2.0",
     description: "Returns the invites disabled timestamp of a guild",
     aliases: [
         "$serverInvitesDisabledUntil"
--- dist/native/message/forward.js
+++ dist/native/message/forward.js
@@ -4,6 +4,7 @@ const discord_js_1 = require("discord.js");
 const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$forward",
+    version: "2.2.0",
     description: "Forwards a message to another channel, returns bool",
     aliases: ["$forwardMessage"],
     unwrap: true,
