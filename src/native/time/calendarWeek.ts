03d7b5fe30e2990b806bdd2a9e5da9ba1087ddd9
Silas <91368591+Daaisukidayo@users.noreply.github.com>
2026-06-27 19:36:27 +0200

    Refactor the calendar week function to support different types of calendars

--- src/native/time/calendarWeek.ts
+++ src/native/time/calendarWeek.ts
@@ -5,19 +5,31 @@
 
 import { ArgType, NativeFunction, Return } from "../../structures"
 
-function getWeekOfYear(date: Date) {
-    const start = new Date(date.getFullYear(), 0, 1)
-    const days = (date.getTime() - start.getTime()) / 86400000
-    return Math.ceil((days + start.getDay() + 1) / 7)
-}
-
 export default new NativeFunction({
     name: "$calendarWeek",
     version: "1.5.0",
-    description: "Returns the calendar week",
+    description: "Returns the calendar week based on calendar type",
     unwrap: true,
     output: ArgType.Number,
     execute: async function(ctx) {
-        return this.success(getWeekOfYear(new Date(new Date().toLocaleString("en-US", { timeZone: ctx.timezone, calendar: ctx.calendar }))))
+        const date = new Date(new Date().toLocaleString("en-US", { timeZone: ctx.timezone, calendar: ctx.calendar }))
+
+        if (ctx.calendar === "iso8601") {
+            const target = new Date(date.valueOf())
+            const dayNr = (date.getDay() + 6) % 7
+            target.setDate(target.getDate() - dayNr + 3)
+            const firstThursday = target.valueOf()
+            target.setMonth(0, 1)
+            if (target.getDay() !== 4) {
+                target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7))
+            }
+            return this.success(1 + Math.ceil((firstThursday - target.valueOf()) / 604800000))
+        }
+
+        const start = new Date(date.getFullYear(), 0, 1)
+        const days = (date.getTime() - start.getTime()) / 86400000
+        const week = Math.ceil((days + start.getDay() + 1) / 7)
+
+        return this.success(week)
     }
 })
